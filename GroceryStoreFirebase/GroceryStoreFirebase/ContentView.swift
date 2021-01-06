f8ac0c74b1c34d53e79c494966a2ed834e45207f
Jason <sanchezjason@me.com>
2021-01-05 23:11:25 -0700

    GS-01 Firebase MVVM

---
+++ GroceryStoreFirebase/GroceryStoreFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GroceryStoreFirebase
+//
+//  Created by Jason Sanchez on 1/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GroceryStoreFirebase/GroceryStoreFirebase/GroceryStoreFirebaseApp.swift
@@ -0,0 +1,21 @@
+//
+//  GroceryStoreFirebaseApp.swift
+//  GroceryStoreFirebase
+//
+//  Created by Jason Sanchez on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFirebaseApp: App {
+    
+    init() {
+        //FirebaseApp.configure()
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
