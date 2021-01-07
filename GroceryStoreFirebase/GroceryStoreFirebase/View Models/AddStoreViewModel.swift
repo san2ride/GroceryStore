8d00df5bab497018d02d9a2ec7fc45fe3928bffb
Jason <sanchezjason@me.com>
2021-01-07 15:21:13 -0700

    GS-05 Implementing AddStoreViewModel

---
+++ GroceryStoreFirebase/GroceryStoreFirebase/View Models/AddStoreViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  AddStoreViewModel.swift
+//  GroceryStoreFirebase
+//
+//  Created by Jason Sanchez on 1/7/21.
+//
+
+import Foundation
+
+class AddStoreViewModel: ObservableObject {
+    
+    private var firestoreManager: FirestoreManager
+    @Published var saved: Bool = false
+    @Published var message: String = ""
+    
+    var name: String = ""
+    var address: String = ""
+    
+    init() {
+        firestoreManager = FirestoreManager()
+    }
+    
+    func save() {
+        
+        let store = Store(name: name, address: address)
+        firestoreManager.save(store: store) { result in
+            switch result {
+                case .success(let store):
+                    DispatchQueue.main.async {
+                        self.saved = store == nil ? false: true
+                    }
+            case .failure(_):
+                DispatchQueue.main.async {
+                    self.message = Constants.Messages.storeSavedFailure
+                }
+            }
+        }
+        
+    }
+}
