0fafe0c7c1b7e3d8326e1b616b11b810cc8d3ca4
Jason <sanchezjason@me.com>
2021-01-07 14:34:15 -0700

    GS-04 Implementing Store Model and Firestore save

---
+++ GroceryStoreFirebase/GroceryStoreFirebase/Models/Store.swift
@@ -0,0 +1,14 @@
+//
+//  Store.swift
+//  GroceryStoreFirebase
+//
+//  Created by Jason Sanchez on 1/7/21.
+//
+
+import Foundation
+
+struct Store: Codable {
+    var id: String?
+    let name: String
+    let address: String
+}
